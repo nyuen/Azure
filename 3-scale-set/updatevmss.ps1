5b01c9c50331a7f4c795af3e3bba84cbdc873f89
Nicolas Yuen <nicolas.yuen@gmail.com>
2017-04-21 16:29:59 +0000

    changing scale direction

--- 3-scale-set/updatevmss.ps1
+++ 3-scale-set/updatevmss.ps1
@@ -1,5 +1,5 @@
-﻿$rgName = "niyuenvmss"
-$vmssName = "demovmssn"
+﻿$rgName = "niyuenvssc"
+$vmssName = "niyuenvss"
 
 #Get info about the scale set
 $vmss = Get-AzureRmVmss -ResourceGroupName $rgName -VMScaleSetName $vmssName
@@ -16,7 +16,7 @@ Remove-AzureRmAutoscaleSetting -ResourceGroup $rgName -Name "cpuautoscale"
 
 #add new autoscale rule
 $rule1 = New-AzureRmAutoscaleRule -MetricName "Percentage CPU" -MetricResourceId $vmss.Id -Operator GreaterThan -MetricStatistic Average -Threshold 60 -TimeGrain 00:01:00 -TimeWindow 00:10:00 -ScaleActionCooldown 00:10:00 -ScaleActionDirection Increase -ScaleActionValue 1
-$rule2 = New-AzureRmAutoscaleRule -MetricName "Percentage CPU" -MetricResourceId $vmss.Id -Operator GreaterThan -MetricStatistic Average -Threshold 30 -TimeGrain 00:01:00 -TimeWindow 00:10:00 -ScaleActionCooldown 00:10:00 -ScaleActionDirection Decrease -ScaleActionValue 1
+$rule2 = New-AzureRmAutoscaleRule -MetricName "Percentage CPU" -MetricResourceId $vmss.Id -Operator LessThan -MetricStatistic Average -Threshold 30 -TimeGrain 00:01:00 -TimeWindow 00:10:00 -ScaleActionCooldown 00:10:00 -ScaleActionDirection Decrease -ScaleActionValue 1
 $profile1 = New-AzureRmAutoscaleProfile -DefaultCapacity 2 -MaximumCapacity 10 -MinimumCapacity 2 -Rules $rule1,$rule2 -Name "My_Profile"
 Add-AzureRmAutoscaleSetting -Location "West Europe" -Name "mynewautoscale" -ResourceGroup $rgName -TargetResourceId $vmss.Id -AutoscaleProfiles $profile1
 
